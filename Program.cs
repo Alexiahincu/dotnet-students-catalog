d34011ee3b2ce26c43f6912d840df5d34871389f
Alexiahincu <Alexialexy31@yahoo.com>
2021-11-01 19:41:37 +0200

    Command Loop Method added

--- Program.cs
+++ Program.cs
@@ -4,9 +4,24 @@ namespace dotnet_students_catalog
 {
     class Program
     {
+        static void EnterCommand()
+        {
+            Console.WriteLine("Enter the command 'exit' to close the program.");
+            bool exit = false;
+            string command = "";
+            while (exit == false)
+            {
+                Console.Write("Enter a command: ");
+                command = Console.ReadLine();
+                if (string.Compare(command, "exit", StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    exit = true;
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! Here I will create a student catalog! :D");
+            EnterCommand();
         }
     }
 }
