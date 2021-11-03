dacf1d73252e98270e98c3d3ddb90f166c6b1c91
Alexiahincu <Alexialexy31@yahoo.com>
2021-11-03 22:54:56 +0200

    Bug fix

--- Program.cs
+++ Program.cs
@@ -4,45 +4,54 @@ namespace dotnet_students_catalog
 {
     class Program
     {
-        static void EnterCommand()
+        static string[] fileInfo = new string[5];
+        static int rowsNumber = 0;
+        static void Main(string[] args)
         {
-            Console.WriteLine("Enter the command 'exit' to close the program.");
-            Console.WriteLine("For more information, enter the command 'about'.");
-            bool exit = false;
-            string command = "";
-            string readPath = "D:\\Repositories\\dotnet-students-catalog\\About.txt";
-            string[] fileInfo = new string[5];
-            int rowsNumber = 0;
+            StartCommandLoop();
+        }
+        static void ExtractFileInfo()
+        {
+            string readPath = "About.txt";
+
             StreamReader read = new StreamReader(readPath);
             while (!read.EndOfStream)
             {
                 fileInfo[rowsNumber] = read.ReadLine();
                 rowsNumber++;
             }
+        }
+        static void ShowInfo()
+        {
+            for (int i = 0; i < rowsNumber; i++)
+            {
+                Console.WriteLine(fileInfo[i]);
+            }
+        }
+        static void StartCommandLoop()
+        {
+            Console.WriteLine("Enter the command 'exit' to close the program.");
+            Console.WriteLine("For more information, enter the command 'about'.");
+            bool exit = false;
+            string command = "";
+            ExtractFileInfo();
             while (exit == false)
             {
                 Console.Write("Enter a command: ");
                 command = Console.ReadLine();
-                if (string.Compare(command, "exit", StringComparison.OrdinalIgnoreCase) == 0)
+                if (string.Compare(command.ToLower(), "exit") == 0)
                 {
                     exit = true;
                 }
-                else if (string.Compare(command, "about", StringComparison.OrdinalIgnoreCase) == 0)
+                else if (string.Compare(command.ToLower(), "about") == 0)
                 {
-                    for (int i = 0; i < rowsNumber; i++)
-                    {
-                        Console.WriteLine(fileInfo[i]);
-                    }
+                    ShowInfo();
                 }
-                else if (string.Compare(command, "costi", StringComparison.OrdinalIgnoreCase) == 0)
+                else if (string.Compare(command.ToLower(), "costi") == 0)
                 {
                     Console.WriteLine("FORTZA!"); //I love easter eggs <3
                 }
             }
-        }
-        static void Main(string[] args)
-        {
-            EnterCommand();
         }
     }
 }
