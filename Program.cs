c1986c9c9a33084b9e65632fe27b4446addf37e7
Alexiahincu <Alexialexy31@yahoo.com>
2021-11-05 01:20:34 +0200

    New Person class

--- Program.cs
+++ Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 namespace dotnet_students_catalog
 {
     class Program
     {
+        static List<Person> personList = new List<Person>();
         static string[] fileInfo = new string[5];
         static int rowsNumber = 0;
+
+        class Person
+        {
+            public string FirstName, LastName;
+            public int Age;
+            public Person(string firstname, string lastname, int age)
+            {
+                FirstName = firstname;
+                LastName = lastname;
+                Age = age;
+            }
+            public void Greet()
+            {
+                Console.WriteLine($"Welcome, {FirstName} {LastName} ({Age})! :)");
+            }
+        }
         static void Main(string[] args)
         {
             StartCommandLoop();
         }
+
         static void ExtractFileInfo()
         {
             string readPath = "About.txt";
@@ -42,24 +61,20 @@ namespace dotnet_students_catalog
                 switch (command.ToLower())
                 {
                     case "about":
-                    {
-                        ShowInfo();
-                        break;
-                    }
+                        {
+                            ShowInfo();
+                            break;
+                        }
                     case "exit":
-                    {
-                        exit=true;
-                        break;
-                    }
+                        {
+                            exit = true;
+                            break;
+                        }
                     case "costi":
-                    {
-                        Console.WriteLine("FORTZA!"); //I love Easter eggs! <3
-                        break;
-                    }
-                    default:
-                    {
-                        break;
-                    }
+                        {
+                            Console.WriteLine("FORTZA!"); //I love Easter eggs! <3
+                            break;
+                        }
                 }
             }
         }
